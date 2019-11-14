9a943084a7a5b84d3183f3e93fa0dc3f17f19913
wanqifeng <47101255+wanqifeng@users.noreply.github.com>
2019-11-14 13:51:06 +0800

    Update demo0.vue

--- components/tab-picker/demo/cases/demo0.vue
+++ components/tab-picker/demo/cases/demo0.vue
@@ -1,27 +1,28 @@
 <template>
   <div class="md-example-child md-example-child-tab-picker md-example-child-tab-picker-0">
-    <md-field title="地址">
-      <md-field-item
-        title="联系地址"
-        arrow="arrow-right"
-        @click="show = !show"
-        placeholder="请选择联系地址"
-        :content="addressStr"
-        solid
-      />
-    </md-field>
-    <md-tab-picker
-      title="请选择"
-      describe="请选择您所在的省份、城市、区县"
-      large-radius
-      :data="data"
-      v-model="show"
-      @change="handleChange"
-    />
-  </div>
+              <md-field>
+                <md-field-item
+                  title="所在省市区"
+                  arrow="arrow-right"
+                  @click="province.show = !province.show"
+                  placeholder="请选择"
+                  :content="addressStr"
+                  solid
+                />
+              </md-field>
+              <md-tab-picker
+                title="请选择"
+                describe="请选择您所在的省份、城市、区县"
+                large-radius
+                :data="province.data"
+                v-model="province.show"
+                @change="handleChange"
+              />
+            </div>
 </template>
 
-<script>import {Field, FieldItem, TabPicker} from 'mand-mobile'
+<script>
+import {Field, FieldItem, TabPicker} from 'mand-mobile'
 import data from 'mand-mobile/components/tab-picker/demo/data'
 
 export default {
@@ -36,20 +37,23 @@ export default {
   },
   data() {
     return {
-      show: false,
-      address: [],
-      data: data,
+      province: {
+          show: false,//是否出现选择器
+          address: [],
+          data: data,
+        }
     }
   },
   computed: {
-    addressStr() {
-      return this.address.map(item => item.label).join(' ')
-    },
+   addressStr() {
+        return this.province.address.map(item => item.label).join(' ')
+      },
   },
   methods: {
     handleChange({options}) {
-      this.address = options
-    },
+        this.province.address = options
+      },
   },
 }
-</script>
+
+</script>
